9212a60e1a073d85160b2be16438738cb4dc7ae1
Garrick Cheung <garrick@garrickcheung.com>
2011-06-15 01:14:18 -0700

    Rewriting Elements.Dataset.js. Provides getData, getDataset, setData, and removeData.

---
+++ Source/Elements/Elements.Dataset.js
@@ -0,0 +1,48 @@
+(function(){
+
+var hasData = (function(){
+	var node = document.createElement('div');
+	node.setAttribute('data-id', '1');
+	return node.dataset && node.dataset.id && node.dataset.id == '1';
+}());
+
+[Element, Document, Window].invoke('implement', {
+    setData: (hasData ? function(key, val){
+        this.dataset[key.camelCase()] = val;
+        return this;
+    } : function(key, val){
+        return this.setProperty('data-' + key.hyphenate(), val);
+    }).overloadSetter(),
+
+    getData: (hasData ? function(key){
+        return this.dataset[key.camelCase()]
+    } : function(key){
+        return this.get('data-' + key.hyphenate());
+    }).overloadGetter(),
+
+    getDataset: hasData ? function(){
+        return Object.clone(this.dataset);
+    } : function(){
+        var attributes = item.attributes,
+            i = attributes.length,
+            collection = {};
+
+        while(i--){
+            var attribute = attributes[ i ],
+                key = attribute.attributeName;
+
+            if (!key.test(/^data-/)) continue;
+
+            key = camelKey(key.replace(/^data-/));
+            collection[ key ] = attribute.value;
+        }
+
+        return collection;
+    },
+
+    removeData: function(key){
+        return this.set('data-' + key.hyphenate(), null);
+    }
+});
+
+}());
