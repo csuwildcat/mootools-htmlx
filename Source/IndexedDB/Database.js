9f1eb88609fb85d6202d7c91d2f5c336546a1e15
Daniel Buchner <danieljb2@gmail.com>
2011-06-30 12:45:43 -0700

    Added keep alive method so that caching of object stores and indexes is possible

--- Source/IndexedDB/Database.js
+++ Source/IndexedDB/Database.js
@@ -19,11 +19,23 @@ provides: Database
 ...
 */
 
-IDBObjectStore.extend = Object.extend;
+(function(){
 
-new Type('IDBObjectStore', IDBObjectStore);
-
-IDBObjectStore.implement(new Chain).implement({
+	var errorTypes = {
+		1: 'unknown',
+		2: 'nonTransient',
+		3: 'notFound',
+		4: 'constraint',
+		5: 'data',
+		6: 'notAllowed',
+		7: 'transactionInactive',
+		8: 'abort',
+		9: 'readonly',
+		10: 'recoverable',
+		11: 'serial',
+		12: 'timeout'
+	},
+	IDBMethods = {
 		store: function(data){
 			this.put((typeof data == 'function') ? data.call(this) : data);
 			return this;
@@ -38,19 +50,19 @@ IDBObjectStore.implement(new Chain).implement({
 			return this;
 		},
 		
-		each: function(fn, bind, internal){
+		each: function(fn, bind, range, relay){
 			var self = this,
 				returned = [],
 				bind = (typeof bind === 'undefined') ? this : bind;
 				
-			this.openCursor().onsuccess = function(e) {
+			this.openCursor(range).onsuccess = function(e) {
 				var data = e.target.result;
 				if(data) {
 					returned.push(fn.call(bind, data.value, data.key, data));
 					data.continue();
 				}
 				else{
-					if(internal) internal.call(self, returned);
+					if(relay) relay.call(bind, returned);
 					self.callChain();
 				}
 			};
@@ -58,139 +70,166 @@ IDBObjectStore.implement(new Chain).implement({
 			return this;
 		},
 		
+		getRange: function(start, end, fn, bind){
+			var range = {};
+			this.each(function(v, k){
+				range[k] = v;
+			}, this, IDBKeyRange.bound(start, end, false, false), fn.bind(bind, range));
+			return this;
+		},
+		
 		getKeys: function(fn){
 			this.each(function(value, key){
 				return key;
-			}, this, fn);
+			}, this, null, fn);
+			return this;
 		},
 		
 		getValues: function(fn){
 			this.each(function(value){
 				return value;
-			}, this, fn);
+			}, this, null, fn);
+			return this;
+		},
+		
+		getIndex: function(name){
+			var index = this.index(name);
+			index.instance = this.instance;
+			return index;
 		}
-});
-
-Database = new Class({
-			
-	Implements: [Options, Events, Chain],
+	},
+	revive = function(object){
+		var revived = (object.objectStore) ? object.instance.getObject(object.objectStore.name).getIndex(object.name) : object.instance.getObject(object.name);
+			revived.$chain = object.$chain;
+		return object = revived;
+	};
 	
-	options: {
-		version: '0.1',
-		schema: [  // This schema is an example and should be removed before release
-			{
-				name: 'users',
-				options: {
-					keyPath: 'id'
-				},
-				indexes: {
-					'UsersByName': {
-						keyPath: 'username',
-						options: {
-							'unique': true
-						}
-					}
-				}
-			},
-			{
-				name: 'projects',
-				options: {
-					keyPath: 'id'
-				},
-				indexes: {
-					'ProjectsByType': {
-						keyPath: 'type'
-					}
-				}
+	[
+		{ type: 'IDBObjectStore', methods: IDBMethods },
+		{ type: 'IDBIndex', methods: Object.subset(IDBMethods, ['retrieve', 'each', 'getRange', 'getKeys', 'getValues']) }
+	].each(function(object){
+		var global = window[object.type];
+		global.extend = Object.extend;
+		new Type(object.type, global);
+		global.implement(new Chain).implement(Object.map(object.methods, function(method){
+			return function(){
+				return method.apply(((this.objectStore || this).inactive) ? revive(this) : this, arguments);
 			}
-		]
-	},
-	
-	initialize: function(name, options){
-		this.setOptions(options);
-		this.name = name;
-		this.open();
-	},
-	
-	errors: {
-		1: 'unknown',
-		2: 'nonTransient',
-		3: 'notFound',
-		4: 'constraint',
-		5: 'data',
-		6: 'notAllowed',
-		7: 'transactionInactive',
-		8: 'abort',
-		9: 'readonly',
-		10: 'recoverable',
-		11: 'serial',
-		12: 'timeout'
-	},
-	
-	open: function(existing){
-		var self = this,
-			options = this.options,
-			request = (mozIndexedDB || webkitIndexedDB).open(this.name);
+		}));
+	});
+
+	Database = new Class({
+				
+		Implements: [Options, Events, Chain],
 		
-		request.onerror = function(error) {
-			self.fireEvent('openFailure', error);
-		}; 
-		
-		request.onblocked = function(){
-			console.log('***BLOCKED***');
-		};
-		
-		request.onsuccess = function(event) {
-			var db = request.result;
-				db.instance = self;
-				self.db = db;
-			
-			db.onversionchange = function(e){
-				if(db.version != self.previousVersion) self.fireEvent('versionChange', e);
-				self.close();
-				self.open(true);
-			};
-			
-			db.onerror = function(e) {
-				self.fireEvent(self.errors[e.target.errorCode], [e, request]);
-			};
-			
-			if(options.schema && !existing) {
-				self.setVersion(options.version || db.version, function(){
-					options.schema.each(function(e){
-						if(!db.objectStoreNames.contains(e.name)) {
-							var object = self.create(e.name, e.options);
-							if(e.indexes){
-								Object.each(e.indexes, function(args, index){
-									object.createIndex(index, args.keyPath, args.options || {});
-								});
+		options: {
+			version: '0.1',
+			schema: [  // This schema is an example and should be removed before release
+				{
+					name: 'users',
+					options: {
+						keyPath: 'id'
+					},
+					indexes: {
+						'UsersByName': {
+							keyPath: 'username',
+							options: {
+								'unique': true
 							}
 						}
-					});
-				});
-			}
+					}
+				},
+				{
+					name: 'projects',
+					options: {
+						keyPath: 'id'
+					},
+					indexes: {
+						'ProjectsByType': {
+							keyPath: 'type'
+						}
+					}
+				}
+			]
+		},
+		
+		initialize: function(name, options){
+			this.setOptions(options);
+			this.name = name;
+			this.open();
+		},
+		
+		open: function(restart){
+			var self = this,
+				options = this.options,
+				request = (mozIndexedDB || webkitIndexedDB).open(this.name);
 			
-			self.fireEvent('open', event);
-		}; 
-	},
-	
-	setVersion: function(version, fn) {
-		this.previousVersion = this.db.version;
-		this.db.setVersion(version || this.db.version).onsuccess = (fn || function(){}).bind(this);
-		return this;
-	},
-	
-	create: function(object, options){
-		return (!this.db.objectStoreNames.contains(object)) ? this.db.createObjectStore(object, options || {}) : this.access(object);
-	},
-	
-	access: function(object){
-		return this.db.transaction([object], IDBTransaction.READ_WRITE, 0).objectStore(object);
-	},
-	
-	close: function(){
-		this.fireEvent('close');
-		this.db.close();
-	}
-	
-});
+			request.onerror = function(error) {
+				self.fireEvent('openFailure', error);
+			}; 
+			
+			request.onblocked = function(){
+				console.log('***BLOCKED***'); // remove console before release
+			};
+			
+			request.onsuccess = function(event) {
+				var db = request.result;
+					db.instance = self;
+					self.db = db;
+				
+				db.onversionchange = function(e){
+					if(db.version != self.previousVersion) self.fireEvent('versionChange', e);
+					self.close();
+					self.open(true);
+				};
+				
+				db.onerror = function(e) {
+					self.fireEvent(errorTypes[e.target.errorCode], [e, request]);
+				};
+				
+				if(options.schema && !restart) {
+					self.setVersion(options.version || db.version, function(){
+						options.schema.each(function(e){
+							if(!db.objectStoreNames.contains(e.name)) {
+								var object = self.create(e.name, e.options);
+								if(e.indexes){
+									Object.each(e.indexes, function(args, index){
+										object.createIndex(index, args.keyPath, args.options || {});
+									});
+								}
+							}
+						});
+					});
+				}
+				
+				self.fireEvent('open', event);
+			}; 
+		},
+		
+		setVersion: function(version, fn) {
+			this.previousVersion = this.db.version;
+			this.db.setVersion(version || this.db.version).onsuccess = (fn || function(){}).bind(this);
+			return this;
+		},
+		
+		create: function(name, options){
+			return(!this.db.objectStoreNames.contains(name)) ? this.db.createObjectStore(name, options || {}) : this.getObject(name);
+		},
+		
+		getObject: function(name){
+			var object = this.db.transaction([name], IDBTransaction.READ_WRITE, 0).objectStore(name);
+			object.instance = this;
+			object.transaction.oncomplete = function(){
+				object.inactive = true;
+			};
+			return object;
+		},
+		
+		close: function(){
+			this.fireEvent('close');
+			this.db.close();
+		}
+		
+	});
+
+})();
